f3b71be0db1e9399803a230d2a7e6c14e7ae221a
Ryan Chen <ryan.chen@chinapex.com>
2021-04-04 23:28:43 +0800

    feat: edit link

--- docs/_nuxt/static/1617548452/article/5c376a7c9f894d0f47f5f53a/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1617548452",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002F5c376a7c9f894d0f47f5f53a",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ docs/_nuxt/static/1617550096/article/5c376a7c9f894d0f47f5f53a/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1617550096",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002F5c376a7c9f894d0f47f5f53a",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
